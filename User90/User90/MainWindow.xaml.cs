73f6dfa129bb0a5fcd312df1e17c17e09a3af54b
alina <alina@DESKTOP-4AP0DOQ>
2024-05-22 11:13:54 +0300

    кнопка

--- User90/User90/MainWindow.xaml.cs
+++ User90/User90/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace User90
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Авторизация авторизация = new Авторизация();
+            авторизация.Show();
+        }
     }
 }
